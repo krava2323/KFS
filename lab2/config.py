ef09c210deffda1a4f84bddfe9208470992b55d0
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-29 00:33:07 +0300

    Create config.py

---
+++ lab2/config.py
@@ -0,0 +1,13 @@
+
+# Конект з базою даних
+
+MONGO_URI = "mongodb://localhost:27017/" 
+MONGO_DATABASE_NAME = "billing_system"
+
+#  Тарифи 
+DAY_TARIFF = 2.64
+NIGHT_TARIFF = 1.32
+
+# Штрафні кіловати у випадку заниження показників
+PENALTY_DAY_KWH = 100.0
+PENALTY_NIGHT_KWH = 80.0
